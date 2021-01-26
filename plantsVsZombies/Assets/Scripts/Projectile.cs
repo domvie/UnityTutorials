b02c7ac7a0d37bf1033bc5e09dd47c744c771e11
Dominic <dominic.viehbock@gmail.com>
2021-01-26 21:58:55 +0100

    projectiles, enemy spawners, collision, health and damage

---
+++ plantsVsZombies/Assets/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    [SerializeField] float damage = 150f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        var colliderHealth = collider.GetComponent<Health>();
+        var attacker = collider.GetComponent<Attacker>();
+
+        if (attacker && colliderHealth)
+        {
+            colliderHealth.DealDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ plantsVsZombies/Assets/Scripts/Shooter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+
+    [SerializeField] GameObject projectile, gun;
+
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, gun.transform.rotation);
+    }
+}
